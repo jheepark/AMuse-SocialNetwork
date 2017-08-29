c98953990df9fc7c62c482299511bc102c0facdf
‘jheepark’ <jhee.park.work@gmail.com>
2017-08-29 10:24:45 +1000

    follow buttons appearing

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -39,6 +39,20 @@ class UsersController < ApplicationController
     redirect_to root_path
   end
 
+  def following
+  @title = "Following"
+  @user  = User.find(params[:id])
+  @users = @user.following.paginate(page: params[:page])
+  render 'show_follow'
+end
+
+def followers
+  @title = "Followers"
+  @user  = User.find(params[:id])
+  @users = @user.followers.paginate(page: params[:page])
+  render 'show_follow'
+end
+
   private
   def user_params
     params.require(:user).permit(:image, :name, :email, :location, :password, :password_confirmation)
