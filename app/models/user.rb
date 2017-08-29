9272e8e00a231ce6290ee556bf87280285e135eb
‘jheepark’ <jhee.park.work@gmail.com>
2017-08-29 12:41:15 +1000

    followers following logic working

--- app/models/user.rb
+++ app/models/user.rb
@@ -24,4 +24,16 @@ class User < ApplicationRecord
                                    dependent:   :destroy
   has_many :following, through: :active_relationships,  source: :followed
   has_many :followers, through: :passive_relationships, source: :follower
+
+  def follow(other_user)
+  following << other_user
+end
+
+def unfollow(other_user)
+  following.delete(other_user)
+end
+
+def following?(other_user)
+  following.include?(other_user)
+end
 end
